23bce4da7dd489875a9f77f29a81dae4f845a4ef
Tyler Hale <t.w.hale32@gmail.com>
2019-05-29 23:06:02 +1200

    Ignore constructor being used for its side effects

--- src/apps/PlotTest/index.js
+++ src/apps/PlotTest/index.js
@@ -2,43 +2,40 @@
 import React, { Component } from 'react'
 import { Jumbotron } from 'reactstrap'
 
-import Chart from "chart.js";
+import Chart from 'chart.js'
 
 class TyChartJS extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {
-    const node = this.node;
+  componentDidMount () {
+    const node = this.node
 
-    var myChart = new Chart(node, {
-      type: "bar",
+    new Chart(node, { // eslint-disable-line
+      type: 'bar',
       data: {
-        labels: ["Red", "Blue", "Yellow"],
+        labels: ['Red', 'Blue', 'Yellow'],
         datasets: [
           {
-            label: "# of Likes",
+            label: '# of Likes',
             data: [12, 19, 3],
             backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)"
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)'
             ]
           }
         ]
       }
-    });
+    })
   }
 
-  render() {
+  render () {
     return (
-        <div>
-          <canvas
-              style={{ width: 800, height: 300 }}
-              ref={node => (this.node = node)}
-          />
-        </div>
-    );
+      <div>
+        <canvas
+          style={{ width: 800, height: 300 }}
+          ref={node => (this.node = node)}
+        />
+      </div>
+    )
   }
 }
 
@@ -47,7 +44,7 @@ class PlotTest extends Component {
   // render
   render () {
     return (<Jumbotron>
-      <h1>ChartJS</h1><TyChartJS/>
+      <h1>ChartJS</h1><TyChartJS />
       <h1>HighCharts</h1> Soon
     </Jumbotron>)
   }
